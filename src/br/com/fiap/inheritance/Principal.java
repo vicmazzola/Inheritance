bfe82a17ff28a01387aee01f9c3a82a198c55bf5
Victor Mazzola <victormazzola2019@gmail.com>
2024-09-07 16:47:21 -0300

    create employee

--- src/br/com/fiap/inheritance/Principal.java
+++ src/br/com/fiap/inheritance/Principal.java
@@ -1,6 +1,7 @@
 package br.com.fiap.inheritance;
 
 import br.com.fiap.inheritance.model.Customer;
+import br.com.fiap.inheritance.model.Employee;
 import br.com.fiap.inheritance.model.People;
 
 import java.time.LocalDate;
@@ -14,6 +15,17 @@ public class Principal {
         c1.setEmail("bobvance@vancerefrigerators.com");
         c1.setPhone("+5511975475677");
         c1.setBirthDate(LocalDate.of(1970, 10, 25));
+
+
+        Employee e1 = new Employee();
+        e1.setName("Michael");
+        e1.setEmail("michael@dundermifflin.com");
+        e1.setPhone("+35699375533");
+        e1.setBirthDate(LocalDate.of(1975, 06, 10));
+        e1.setSalary(18900.00);
+        e1.setRole("Manager");
+
+
 
     }
 
---
+++ src/br/com/fiap/inheritance/model/Employee.java
@@ -0,0 +1,23 @@
+package br.com.fiap.inheritance.model;
+
+public class Employee extends People {
+
+    private double salary;
+    private String role;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
