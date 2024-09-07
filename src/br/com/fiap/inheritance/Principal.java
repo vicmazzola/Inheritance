c40fbc0da63cb97c3beb4e72af4aa990fdd095c8
Victor Mazzola <victormazzola2019@gmail.com>
2024-09-07 15:40:49 -0300

    Start project

--- src/br/com/fiap/inheritance/Principal.java
+++ src/br/com/fiap/inheritance/Principal.java
@@ -1,8 +1,12 @@
 package br.com.fiap.inheritance;
 
+import br.com.fiap.inheritance.model.People;
+
 public class Principal {
 
     public static void main(String[] args) {
+
+        People Victor = new People();
 
 
 
