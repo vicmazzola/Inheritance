e2b37bbc6b4abee3bda6f0428e202368397c4b0d
Victor Mazzola <victormazzola2019@gmail.com>
2024-09-07 15:57:55 -0300

    Start project

--- src/br/com/fiap/inheritance/Principal.java
+++ src/br/com/fiap/inheritance/Principal.java
@@ -2,12 +2,16 @@ package br.com.fiap.inheritance;
 
 import br.com.fiap.inheritance.model.People;
 
+import java.time.LocalDate;
+
 public class Principal {
 
     public static void main(String[] args) {
 
-        People Victor = new People();
+        People p1 = new People();
 
+        p1.setName("Victor");
+        p1.setBirthDate(LocalDate.of(1997, 6, 10));
 
 
     }
